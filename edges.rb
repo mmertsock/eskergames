033414f36643cb9274ed74164524bf32f2760668
Mike Mertsock <mike.mertsock@gmail.com>
2019-01-29 10:24:45 -0500

    Animation frame support in edge generator

--- edges.rb
+++ edges.rb
@@ -19,6 +19,43 @@ end
 
 base_files = Dir.glob(File.join(srcdir, '*-base*.png'))
 
+class SpriteRow
+    attr_reader :imgs
+
+    def initialize(pixel_size, img_array = nil, file_path = nil)
+        @pixel_size = pixel_size
+        if file_path == nil
+            @imgs = img_array
+        else
+            row = ChunkyPNG::Image.from_file(file_path)
+            fail("Unexpected image size #{row.width} x #{row.height} for #{file_path}") if row.height != @pixel_size
+            fail("Unexpected image aspect ratio #{row.width} x #{row.height} for #{file_path}") if row.width % row.height != 0
+            frames = row.width / row.height
+            @imgs = Array.new(frames) { |i| row.crop(i * @pixel_size, 0, @pixel_size, @pixel_size) }
+        end
+    end
+
+    def map(&block)
+        SpriteRow.new(@pixel_size, @imgs.map(&block))
+    end
+
+    def frame_count
+        @imgs.size
+    end
+
+    def frame(counter)
+        @imgs[counter % @imgs.size]
+    end
+
+    def save(file_path)
+        canvas = ChunkyPNG::Image.new(@pixel_size * @imgs.size, @pixel_size)
+        @imgs.each_with_index do |img, index|
+            canvas.compose!(img, index * @pixel_size, 0)
+        end
+        canvas.save(file_path)
+    end
+end
+
 class EdgeGenerator
     def initialize(base_file_path)
         (dirpath, @base_file_name) = File.split(base_file_path)
@@ -29,7 +66,7 @@ class EdgeGenerator
         @sprite_id = name_parts.first.chomp("-base")
 
         # not supporting animation yet
-        @base_img = ChunkyPNG::Image.from_file(base_file_path).crop(0, 0, @pixel_size, @pixel_size)
+        @base_img = SpriteRow.new(@pixel_size, nil, base_file_path)
         @edge_imgs = {}
         load_edge_rotations(dirpath, "N", ["straight-n", "straight-e", "straight-s", "straight-w"])
         load_edge_rotations(dirpath, "NE", ["angle-ne", "angle-se", "angle-sw", "angle-nw"])
@@ -45,24 +82,21 @@ class EdgeGenerator
     end
 
     def to_s
-        "EdgeGenerator #{@sprite_id} w#{@pixel_size}"
+        "EdgeGenerator #{@sprite_id}_#{@pixel_size}"
     end
 
     def load_edge_rotations(dirpath, type, keys)
-        name = "#{@sprite_id}-edge-#{type}_#{@pixel_size}.png"
-        img = ChunkyPNG::Image.from_file(File.join(dirpath, name)).crop(0, 0, @pixel_size, @pixel_size)
-        @edge_imgs[keys[0]] = img
-        @edge_imgs[keys[1]] = img.rotate_right
-        @edge_imgs[keys[2]] = img.rotate_180
-        @edge_imgs[keys[3]] = img.rotate_left
+        path = File.join(dirpath, "#{@sprite_id}-edge-#{type}_#{@pixel_size}.png")
+        row = SpriteRow.new(@pixel_size, nil, path)
+        @edge_imgs[keys[0]] = row
+        @edge_imgs[keys[1]] = row.map { |img| img.rotate_right }
+        @edge_imgs[keys[2]] = row.map { |img| img.rotate_180 }
+        @edge_imgs[keys[3]] = row.map { |img| img.rotate_left }
     end
 
     def process(destdir)
-        puts to_s
+        puts "Process #{to_s}"
 
-        puts "TODO LIST"
-        puts "Support animation frames"
-        puts "Clean up wavefront overlaps"
         # eg. see the 0xff example: NW wavefront is fully visible (and NE is not)
         # could use ChunkyPNG mask functionality? mask using top left pixel color of the N edge image?
 
@@ -70,7 +104,7 @@ class EdgeGenerator
         mockup_sample_height = @pixel_size * 3 * @mockup_sample_cols
 
         mockup_canvas = ChunkyPNG::Image.new(mockup_sample_width, mockup_sample_height)
-        256.times { |index| make_mockup(mockup_canvas, index) }
+        256.times { |index| make_mockup(mockup_canvas, index, 0) }
         mockup_canvas.save(File.join(destdir, @mockup_canvas_name))
 
         # puts "Variants:"
@@ -87,7 +121,7 @@ class EdgeGenerator
     end
 
     # each mockup is a 3x3 tile space, so you can mark up the neighbors
-    def make_mockup(canvas, index)
+    def make_mockup(canvas, index, frame)
         (row, col) = index.divmod(@mockup_sample_cols)
         width = @pixel_size * 3
         height = @pixel_size * 3
@@ -99,7 +133,7 @@ class EdgeGenerator
         neighbors.fill_int(index)
         neighbors.values.each_index do |i|
             if neighbors.is_edge_index_center?(i)
-                compose_tile(canvas, neighbors, x + @pixel_size, y + @pixel_size, idlog)
+                compose_tile(canvas, neighbors, frame, x + @pixel_size, y + @pixel_size, idlog)
             else
                 if neighbors.values[i]
                     color = ChunkyPNG::Color.from_hsl(33, 0.33, 0.8) #dirt
@@ -127,33 +161,33 @@ class EdgeGenerator
         end
     end
 
-    def compose_tile(canvas, neighbors, x, y, idlog)
-        canvas.compose!(@base_img, x, y)
+    def compose_tile(canvas, neighbors, frame, x, y, idlog)
+        canvas.compose!(@base_img.frame(frame), x, y)
 
         compose_depth_gradient(canvas, neighbors, x, y)
 
         # add shallow diagonal edges (special case)
-        compose_edge(canvas, idlog, "corner-ne", x, y, neighbors.NE && !(neighbors.N || neighbors.E))
-        compose_edge(canvas, idlog, "corner-se", x, y, neighbors.SE && !(neighbors.E || neighbors.S))
-        compose_edge(canvas, idlog, "corner-sw", x, y, neighbors.SW && !(neighbors.S || neighbors.W))
-        compose_edge(canvas, idlog, "corner-nw", x, y, neighbors.NW && !(neighbors.N || neighbors.W))
+        compose_edge(canvas, idlog, "corner-ne", frame, x, y, neighbors.NE && !(neighbors.N || neighbors.E))
+        compose_edge(canvas, idlog, "corner-se", frame, x, y, neighbors.SE && !(neighbors.E || neighbors.S))
+        compose_edge(canvas, idlog, "corner-sw", frame, x, y, neighbors.SW && !(neighbors.S || neighbors.W))
+        compose_edge(canvas, idlog, "corner-nw", frame, x, y, neighbors.NW && !(neighbors.N || neighbors.W))
 
         # add straight-line edges
-        compose_edge(canvas, idlog, "straight-n", x, y, neighbors.N)
-        compose_edge(canvas, idlog, "straight-e", x, y, neighbors.E)
-        compose_edge(canvas, idlog, "straight-s", x, y, neighbors.S)
-        compose_edge(canvas, idlog, "straight-w", x, y, neighbors.W)
+        compose_edge(canvas, idlog, "straight-n", frame, x, y, neighbors.N)
+        compose_edge(canvas, idlog, "straight-e", frame, x, y, neighbors.E)
+        compose_edge(canvas, idlog, "straight-s", frame, x, y, neighbors.S)
+        compose_edge(canvas, idlog, "straight-w", frame, x, y, neighbors.W)
 
         # add deep diagonal edges
-        compose_edge(canvas, idlog, "angle-ne", x, y, neighbors.N && neighbors.E)
-        compose_edge(canvas, idlog, "angle-se", x, y, neighbors.E && neighbors.S)
-        compose_edge(canvas, idlog, "angle-sw", x, y, neighbors.S && neighbors.W)
-        compose_edge(canvas, idlog, "angle-nw", x, y, neighbors.N && neighbors.W)
+        compose_edge(canvas, idlog, "angle-ne", x, frame, y, neighbors.N && neighbors.E)
+        compose_edge(canvas, idlog, "angle-se", x, frame, y, neighbors.E && neighbors.S)
+        compose_edge(canvas, idlog, "angle-sw", x, frame, y, neighbors.S && neighbors.W)
+        compose_edge(canvas, idlog, "angle-nw", x, frame, y, neighbors.N && neighbors.W)
     end
 
-    def compose_edge(canvas, idlog, img_key, x, y, condition)
+    def compose_edge(canvas, idlog, img_key, frame, x, y, condition)
         if condition
-            canvas.compose!(@edge_imgs[img_key], x, y)
+            canvas.compose!(@edge_imgs[img_key].frame(frame), x, y)
             if idlog
                 idlog.push(img_key)
             end
@@ -208,13 +242,18 @@ class EdgeGenerator
         @variants.each_with_index do |edge_list, variant_key|
             file_name = "#{@sprite_id}_#{variant_key}_#{@pixel_size}.png"
             # puts "Save file #{file_name} with edges #{edge_list}"
-            img = ChunkyPNG::Image.new(@pixel_size, @pixel_size)
-            img.compose!(@base_img, 0, 0)
-            compose_depth_gradient(img, @variant_neighborhoods[variant_key], 0, 0)
-            edge_list.split("|").each do |img_key|
-                img.compose!(@edge_imgs[img_key], 0, 0)
+            frame_index = 0
+            row = @base_img.map do |base_frame|
+                img = ChunkyPNG::Image.new(@pixel_size, @pixel_size)
+                img.compose!(base_frame, 0, 0)
+                compose_depth_gradient(img, @variant_neighborhoods[variant_key], 0, 0)
+                edge_list.split("|").each do |img_key|
+                    img.compose!(@edge_imgs[img_key].frame(frame_index), 0, 0)
+                end
+                frame_index = frame_index + 1
+                img
             end
-            img.save(File.join(destdir, file_name))
+            row.save(File.join(destdir, file_name))
         end
         puts "Generated #{@variants.size} variants for spritesheet.rb processing."
     end
@@ -305,6 +344,7 @@ class EdgeGrid
     end
 end
 
+puts "TODO Clean up wavefront overlaps"
 base_files.each do |file|
     EdgeGenerator.new(file).process(destdir)
 end
