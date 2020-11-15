21d1fee62ffba30de6895911b8ab10d6a379b0f7
Mike Mertsock <mike.mertsock@gmail.com>
2020-11-15 18:04:06 -0500

    deploy script

---
+++ deploy.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+DEST=$1
+
+cp charts.js "$DEST"
+cp city-content.yaml "$DEST"
+cp city-core.js "$DEST"
+cp city-icon-180.png "$DEST"
+cp city-icon-192.png "$DEST"
+cp city-index.js "$DEST"
+cp city.css "$DEST"
+cp city.html "$DEST"
+cp city.js "$DEST"
+cp g.js "$DEST"
+cp game-content.js "$DEST"
+cp index.html "$DEST"
+cp js-yaml.min.js "$DEST"
+cp locale.js "$DEST"
+cp sprites.html "$DEST"
+cp sprites.js "$DEST"
+cp -R spritesheets/ $DEST/spritesheets/
+cp sweep-content.yaml "$DEST"
+cp sweep-solver.js "$DEST"
+cp sweep.html "$DEST"
+cp sweep.js "$DEST"
+cp terrain.html "$DEST"
+cp terrain.js "$DEST"
+cp tests.html "$DEST"
+cp tests.js "$DEST"
+
+echo "$(date --iso-8601=seconds) deploy.sh to $DEST" >> ~/logs/deploy-eskergames.txt
